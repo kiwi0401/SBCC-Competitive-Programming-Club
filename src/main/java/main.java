09191015eeefc4bb86072dc1203c9ad75af15422
kiwi0401 <kiwi04011@gmail.com>
2021-09-09 20:52:29 -0500

    initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class main {
+    public static void main(String[]args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
